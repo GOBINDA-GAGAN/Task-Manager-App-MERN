a97e3de0b760db8ce70ad8a2ef7cf4c0a4222863
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-04-01 17:01:27 +0530

    create  user and task schema

---
+++ Backend/models/Task.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    priority: {
+      type: String,
+      enum: ["High", "Medium", "Low"],
+      default: "Medium",
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "In Progress", "Completed"],
+      default: "Pending",
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    attachments: [
+      {
+        type: String,
+      },
+    ],
+    todoCheckList: [todoSchema],
+    progress: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
