fb46508f7c0522e6b0f540abc77389b1c349909f
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-04-01 16:28:45 +0530

    create  route in backend and all  folder & file

---
+++ Backend/README.md
@@ -0,0 +1,11 @@
+# Install All dependencies
+
+- express – Web framework for Node.js
+- jsonwebtoken – Handles JWT authentication
+- mongoose – ODM for MongoDB
+- multer – Middleware for handling file uploads
+- bcryptjs – For password hashing
+- cors – Enables Cross-Origin Resource Sharing
+- dotenv – Loads environment variables from a .env file
+- excel-js – Library for working with Excel files in Node.js
+- nodemon - restart the server automatically
---
+++ Backend/server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+//Middleware to handel cors requests
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Middleware
+app.use(express.json());
+
+//Routers
+
+// app.use("/api/auth",authRoutes);
+// app.use("/api/users",userRoutes);
+// app.use("/api/task",taskRoutes);
+// app.use("/api/reports",reportsRoutes);
+
+
+//check the router
+app.get("/", (req, res) => {
+  res.send({
+    message: "Task Manager API",
+    version: "1.0.0",
+    author: "Gobinda Gagan Dey",
+    license: "MIT",
+  });
+});
+
+//Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
