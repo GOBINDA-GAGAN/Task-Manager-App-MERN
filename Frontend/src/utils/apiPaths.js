ddb477c27c239c83955a49f969e3cfcc5556a69c
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-04-17 20:14:48 +0530

    add auth path

--- Frontend/src/utils/apiPaths.js
+++ Frontend/src/utils/apiPaths.js
@@ -1,2 +1,9 @@
-export const BASE_URL="http://localhost:8080"
+export const BASE_URL = "http://localhost:8080";
 
+export const API_PATH = {
+  AUTH: {
+    REGISTER: "/api/auth/register",
+    LOGIN: "/api/auth/login",
+    GET_PROFILE: "/api/auth/profile",
+  },
+};
