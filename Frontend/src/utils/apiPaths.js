f89bf72a5ab210e59b882d7557d06418ecc77fb8
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-04-17 20:36:02 +0530

    create  users Api path

--- Frontend/src/utils/apiPaths.js
+++ Frontend/src/utils/apiPaths.js
@@ -10,15 +10,18 @@ export const API_PATH = {
     GET_DASHBOARD_DATA: "/api/task/dashboard-data",
     GET_USER_DASHBOARD_DATA: "/api/task/user-dashboard-data",
     GET_ALL_TASKS: "/api/task/",
-    GET_TASK_BY_ID: (taskId = `/api/task/${taskId}`),
+    GET_TASK_BY_ID: (taskId) => `/api/task/${taskId}`,
     CREATE_TASK: "/api/task/",
-    UPDATE_TASK: (taskId = `/api/task/${taskId}`),
-    DELETE_TASK: (taskId = `/api/task/${taskId}`),
-    UPDATE_TASK_STATUS: (taskId = `/api/task/${taskId}/status`),
-    UPDATE_TASK_TODO: (taskId = `/api/task/${taskId}/todo`),
+    UPDATE_TASK: (taskId) => `/api/task/${taskId}`,
+    DELETE_TASK: (taskId) => `/api/task/${taskId}`,
+    UPDATE_TASK_STATUS: (taskId) => `/api/task/${taskId}/status`,
+    UPDATE_TASK_TODO: (taskId) => `/api/task/${taskId}/todo`,
   },
-
-
-
-
+  USERS: {
+    GET_ALL_USER: "/api/users",
+    GET_USER_BY_ID: (userId) => `/api/users/${userId}`,
+    CREATE_USER: "/api/users",
+    UPDATE_USER: (userId) => `/api/users/${userId}`,
+    DELETE_USER: (userId) => `/api/users/${userId}`,
+  },
 };
