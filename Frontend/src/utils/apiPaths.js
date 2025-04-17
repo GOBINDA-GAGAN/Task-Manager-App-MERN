3db5856aeabe7c59355f6c4e2b4a364b0dbbf82b
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-04-17 20:24:06 +0530

    add the task paht

--- Frontend/src/utils/apiPaths.js
+++ Frontend/src/utils/apiPaths.js
@@ -6,4 +6,15 @@ export const API_PATH = {
     LOGIN: "/api/auth/login",
     GET_PROFILE: "/api/auth/profile",
   },
+  TASK: {
+    GET_DASHBOARD_DATA: "/api/task/dashboard-data",
+    GET_USER_DASHBOARD_DATA: "/api/task/user-dashboard-data",
+    GET_ALL_TASKS: "/api/task/",
+    GET_TASK_BY_ID: (taskId = `/api/task/${taskId}`),
+    CREATE_TASK: "/api/task/",
+    UPDATE_TASK: (taskId = `/api/task/${taskId}`),
+    DELETE_TASK: (taskId = `/api/task/${taskId}`),
+    UPDATE_TASK_STATUS: (taskId = `/api/task/${taskId}/status`),
+    UPDATE_TASK_TODO: (taskId = `/api/task/${taskId}/todo`),
+  },
 };
