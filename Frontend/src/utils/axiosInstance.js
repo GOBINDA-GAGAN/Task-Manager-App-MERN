63c7c99cdec56f952ecb99cbcd2368a0df2ccfab
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-04-18 21:21:46 +0530

    create  request interceptors

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -9,3 +9,16 @@ const axiosInstance = axios.create({
   },
 });
 
+// request interceptors
+axios.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
