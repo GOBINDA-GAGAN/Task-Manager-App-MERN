1f0fcff591a894aca4dbaff8d5b230372f56d2c2
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-04-19 22:42:44 +0530

    fetchUser function

--- Frontend/src/context/userContext.jsx
+++ Frontend/src/context/userContext.jsx
@@ -4,6 +4,38 @@ import { API_PATH } from "../utils/apiPaths";
 
 export const userContext = createContext();
 
-const useProvider = ({ Children }) => {};
+const useProvider = ({ Children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (user) return;
+
+    const accessToken = localStorage.getItem("token");
+    if (!accessToken) {
+      setLoading(false);
+      return;
+    }
+
+    const fetchUser = async () => {
+      try {
+        const response = await axiosInstance.get(API_PATH.AUTH.GET_PROFILE);
+        setUser(response.data);
+      } catch (error) {
+        console.error("user not authenticate", error);
+        clearUser();
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  },[]);
+
+
+
+
+
+
+};
 
 export default useProvider;
