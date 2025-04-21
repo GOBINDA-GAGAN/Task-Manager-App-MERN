e80e49ad99262dd69884e876d6d54a9419648dcd
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-04-21 19:09:20 +0530

    net start MongoDB in cmd use for fixed the error

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -10,3 +10,6 @@
    }
  }
  module.exports = connectDB;
+
+ // mongodb error-> Error connecting to MongoDB 🔴: connect ECONNREFUSED ::1:27017, connect ECONNREFUSED 127.0.0.1:27017
+// fixed-> cmd->net start MongoDB
